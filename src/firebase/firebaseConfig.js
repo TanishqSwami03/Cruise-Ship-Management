dd9b2e6625db46ce0a2f558029f31441060ede85
Tanishq Raj Swami <tanishqrajswami@gmail.com>
2025-05-24 23:32:50 +0530

    gitignore updated

--- src/firebase/firebaseConfig.js
+++ src/firebase/firebaseConfig.js
@@ -3,15 +3,14 @@ import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBQyBwEC4Ndp_8jmCQiP7-beKqHf22KdFY",
-  authDomain: "cruise-ship-management-trs.firebaseapp.com",
-  projectId: "cruise-ship-management-trs",
-  storageBucket: "cruise-ship-management-trs.firebasestorage.app",
-  messagingSenderId: "75437992996",
-  appId: "1:75437992996:web:aee0f13057b01425663cb1"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID
 };
 
-// Initialize Firebase
 export const app = initializeApp(firebaseConfig);
 export const db = getFirestore(app);
 export const auth = getAuth(app);
